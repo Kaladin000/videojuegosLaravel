9580516016bfe5c4b6cb68130778cf94184529be
David García Valero <davidgarciavalero@gmail.com>
2026-02-12 15:51:52 +0100

    Add update and destroy functionality to VideojuegoController
    Implement update and destroy methods for Videojuego.

--- app/Http/Controllers/VideojuegoController.php
+++ app/Http/Controllers/VideojuegoController.php
@@ -72,15 +72,52 @@ class VideojuegoController extends Controller
      * Update the specified resource in storage.
      */
     public function update(UpdateVideojuegoRequest $request, Videojuego $videojuego)
-    {
-        //
-    }
+{
+    $videojuego->titulo = $request->titulo ?? $videojuego->titulo;
+
+    $videojuego->descripcion = $request->descripcion ?? $videojuego->descripcion;
+
+    $videojuego->numero = $request->numero ?? $videojuego->numero;
+
+    $videojuego->genero = $request->genero ?? $videojuego->genero;
+
+    $videojuego->subgenero = $request->subgenero ?? $videojuego->subgenero;
+
+    $videojuego->mes = $request->mes ?? $videojuego->mes;
+
+    $videojuego->año = $request->año ?? $videojuego->año;
+
+    $videojuego->desarrollador_id = $request->desarrollador_id ?? $videojuego->desarrollador_id;
+
+    $videojuego->editor_id = $request->editor_id ?? $videojuego->editor_id;
+
+    $videojuego->save();
+
+    return response([
+        "error" => false,
+        "message" => "Videojuego modificado",
+        "data" => $videojuego,
+        "code" => 200
+    ], 200);
+}
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Videojuego $videojuego)
     {
-        //
+        if (!$videojuego->delete()) {
+            return response([
+                "error"=>true,
+                "message"=>"El videojuego no se pudo eliminar.",
+                "code"=>500
+            ], 500);
+        } else {
+            return response([
+                "error"=>false,
+                "message"=>"El videojuego se ha eliminado correctamente.",
+                "code"=>200
+            ], 200);
+        }
     }
 }
